d62aa0f0e4947806486b19a9e1b34075786737c1
AJAHN1999 <tundlaajahn@gmail.com>
2024-02-13 14:33:39 +1100

    initial idea

---
+++ iffa.js
@@ -0,0 +1,12 @@
+const enterSiteButton = document.querySelector(".enterSite");
+enterSiteButton.addEventListener("click",()=>{
+    window.location.href = "./home.html"
+})
+
+const submitButton = document.querySelector(".submitButton");
+submitButton.addEventListener("click",displayForm);
+
+function displayForm(){
+    const form = document.querySelector(".form");
+    
+}
