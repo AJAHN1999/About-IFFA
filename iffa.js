05c219efd8d7fd963d8057264693aa036fc81a89
AJAHN1999 <103020052+AJAHN1999@users.noreply.github.com>
2024-02-15 12:54:52 +1100

    this commit contains the inital home page

--- iffa.js
+++ iffa.js
@@ -1,12 +1,16 @@
-const enterSiteButton = document.querySelector(".enterSite");
-enterSiteButton.addEventListener("click",()=>{
-    window.location.href = "./home.html"
-})
+// const enterSiteButton = document.querySelector(".enterSite");
+// enterSiteButton.addEventListener("click",()=>{
+//     window.location.href = "./home.html"
+// })
 
-const submitButton = document.querySelector(".submitButton");
-submitButton.addEventListener("click",displayForm);
+// const submitButton = document.querySelector(".submitButton");
+// submitButton.addEventListener("click",displayForm);
 
-function displayForm(){
-    const form = document.querySelector(".form");
+// function displayForm(){
+//     const form = document.querySelector(".form");
     
-}
+// }
+
+$(document).ready(function(){
+    $('.carousel').carousel({interval:2000});
+});
