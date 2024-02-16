c19fc6abac57e18be6a528a3079d6b55b35b8ff8
AJAHN1999 <103020052+AJAHN1999@users.noreply.github.com>
2024-02-16 14:06:56 +1100

    this commit is my first draft of my final website,still missing a few functionalities

--- iffa.js
+++ iffa.js
@@ -14,3 +14,28 @@
 $(document).ready(function(){
     $('.carousel').carousel({interval:2000});
 });
+
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        stagePadding:50,
+        loop:true,
+        margin:10,
+        nav:true,
+        items: 3,
+        // responsive:{
+        //     0:{
+        //         items:1,
+        //         nav:true
+        //     },
+        //     480:{
+        //         items:3,
+        //         nav:false
+        //     },
+        //     768:{
+        //         items:5,
+        //         nav:true,
+        //         loop:false
+        //     }
+        // }
+});
+  });
